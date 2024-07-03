dbfa6c847c58c742c50bf99375924a5ac81f6208
Tom Bagiau <bagiautom@gmail.com>
2024-07-03 17:13:25 -0400

    navigation setup

---
+++ app/players.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const players = () => {
+  return (
+    <View>
+      <Text>players</Text>
+    </View>
+  )
+}
+
+export default players
+
+const styles = StyleSheet.create({})
