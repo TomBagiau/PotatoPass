e539653bc10e6175359933e7d9983bb76ed62b9c
Tom Bagiau <bagiautom@gmail.com>
2024-07-03 19:04:42 -0400

    homepage

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router' 
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Redirect, router } from 'expo-router' 
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants'
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-abril">PotatoPass</Text>
-      <StatusBar style="auto" />
-      <Link href="/players" style={{ color: 'blue'}}>Go To Players</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-center items-center h-full px-4">
+
+          <Image 
+            source={images.logoPotato}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode='contain'
+          />
+
+          <CustomButton
+            title="Jouer" 
+            handlePress={() => router.push('/players')}
+            containerStyles="w-full mt-7"
+            textStyles="text-3xl"
+          />
+
+          <CustomButton
+            title="Règles" 
+            handlePress={() => router.push('/rules')}
+            containerStyles="w-full mt-7"
+            textStyles="text-2xl"
+          />
+
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor='#EDEBDC' style='dark' />
+    </SafeAreaView>
   );
 }
