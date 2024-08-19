2a3b30f6f63369be681a1eea64b8019a557b483c
Tom Bagiau <bagiautom@gmail.com>
2024-08-19 18:39:27 -0400

    orientation screen gameplay

--- app/play.jsx
+++ app/play.jsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 const play = () => {
-  return (
-    <View>
-      <Text>play</Text>
-    </View>
-  )
-}
+  useEffect(() => {
+    // Verrouiller l'orientation en paysage
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
 
-export default play
+    // Déverrouiller l'orientation lorsque le composant est démonté
+    return () => {
+      ScreenOrientation.unlockAsync();
+    };
+  }, []);
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <View className="w-full justify-center items-center h-full px-4">
+        <Text>Play</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default play;
